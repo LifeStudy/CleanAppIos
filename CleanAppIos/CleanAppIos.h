bec6755615f46551efb87eed8d2844150e1d1dae
ArjDev69 <bruno96araujo@gmail.com>
2021-06-15 15:26:39 -0300

    Initial Commit

---
+++ CleanAppIos/CleanAppIos.h
@@ -0,0 +1,18 @@
+//
+//  CleanAppIos.h
+//  CleanAppIos
+//
+//  Created by bruno araujo on 15/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CleanAppIos.
+FOUNDATION_EXPORT double CleanAppIosVersionNumber;
+
+//! Project version string for CleanAppIos.
+FOUNDATION_EXPORT const unsigned char CleanAppIosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanAppIos/PublicHeader.h>
+
+
