49b3318e7702ecaf13af6dea40e8bc4bf102cc3a
ArjDev69 <bruno96araujo@gmail.com>
2021-06-16 12:00:04 -0300

    change AddAccount and HttpPostClient to use Result instead of error

--- Data/UseCases/RemoteAddAccount.swift
+++ Data/UseCases/RemoteAddAccount.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Domain
 
-public final class RemoteAddAccount {
+public final class RemoteAddAccount: AddAccount {
     private let url: URL
     private let httpClient: HttpPostClient
     
@@ -17,7 +17,9 @@ public final class RemoteAddAccount {
         self.httpClient = httpClient
     }
     
-    public func add(addAccountModel: AddAccountModel, completion: @escaping (DomainError) -> Void) {
-        httpClient.post(to: url, with: addAccountModel.toData()) { error in completion(.unexpected) }
+    public func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, DomainError>) -> Void) {
+        httpClient.post(to: url, with: addAccountModel.toData()) { error in
+            completion(.failure(.unexpected))
+        }
     }
 }
