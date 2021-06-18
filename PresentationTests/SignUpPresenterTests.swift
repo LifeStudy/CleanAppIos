db5d5ed545821c27f41b0f1b0a8d2427d211bb0d
ArjDev69 <bruno96araujo@gmail.com>
2021-06-18 11:16:25 -0300

    add presentation layer

---
+++ PresentationTests/SignUpPresenterTests.swift
@@ -0,0 +1,16 @@
+
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by bruno araujo on 18/06/21.
+//
+
+import XCTest
+
+class SignUpPresenterTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
