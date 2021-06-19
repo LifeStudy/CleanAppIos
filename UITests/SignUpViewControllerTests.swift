93b7fbfc20559f7609e8c5d094a7e8e0058927cd
ArjDev69 <bruno96araujo@gmail.com>
2021-06-19 16:54:01 -0300

    add ui layer

---
+++ UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,12 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by bruno araujo on 19/06/21.
+//
+
+import XCTest
+
+class SignUpViewControllerTests: XCTestCase {
+    func testExample() throws {}
+}
