c8289fd54456486b9228d58d55d3cc3a7f3c26dc
ArjDev69 <bruno96araujo@gmail.com>
2021-06-21 16:37:12 -0300

    move EmailValidatorAdapter to its own file

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -4,27 +4,30 @@
 //
 //  Created by bruno araujo on 21/06/21.
 //
-
 import XCTest
 import Presentation
-
-public final class EmailValidatorAdapter: EmailValidator {
-    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-    
-    public func isValid(email: String) -> Bool {
-        let range = NSRange(location: 0, length: email.utf16.count)
-        let regex = try! NSRegularExpression(pattern: pattern)
-        return regex.firstMatch(in: email, options: [], range: range) != nil
-    }
-}
+import Validation
 
 class EmailValidatorAdapterTests: XCTestCase {
     func test_invalid_emails() {
-        let sut = EmailValidatorAdapter()
+        let sut = makeSut()
         XCTAssertFalse(sut.isValid(email: "rr"))
         XCTAssertFalse(sut.isValid(email: "rr@"))
         XCTAssertFalse(sut.isValid(email: "rr@rr"))
         XCTAssertFalse(sut.isValid(email: "rr@rr."))
         XCTAssertFalse(sut.isValid(email: "@rr.com"))
+    }
+
+    func test_valid_emails() {
+        let sut = makeSut()
+        XCTAssertTrue(sut.isValid(email: "rodrigo@gmail.com"))
+        XCTAssertTrue(sut.isValid(email: "rodrigo@hotmail.com"))
+        XCTAssertTrue(sut.isValid(email: "rodrigo@hotmail.com"))
+    }
+}
+
+extension EmailValidatorAdapterTests {
+    func makeSut() -> EmailValidatorAdapter {
+        return EmailValidatorAdapter()
     }
 }
