d1d501382aa046eb8bb3d6136eb4b474cb6de27d
ArjDev69 <bruno96araujo@gmail.com>
2021-06-21 11:23:06 -0300

    ensure SignUpViewController calls signUp with correct values

--- UI/SignUpViewController.swift
+++ UI/SignUpViewController.swift
@@ -4,25 +4,35 @@
 //
 //  Created by bruno araujo on 21/06/21.
 //
-
 import Foundation
 import UIKit
 import Presentation
+
 final class SignUpViewController: UIViewController {
     @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmationTextField: UITextField!
+    
     var signUp: ((SignUpViewModel) -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
     }
+    
     private func configure() {
         saveButton?.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
     }
+    
     @objc private func saveButtonTapped() {
-        signUp?(SignUpViewModel(name: nil, email: nil, password: nil, passwordConfirmation: nil))
+        let viewModel = SignUpViewModel(name: nameTextField?.text, email: emailTextField?.text, password: passwordTextField?.text, passwordConfirmation: passwordConfirmationTextField?.text)
+        signUp?(viewModel)
     }
 }
+
 extension SignUpViewController: LoadingView {
     func display(viewModel: LoadingViewModel) {
         if viewModel.isLoading {
@@ -32,7 +42,9 @@ extension SignUpViewController: LoadingView {
         }
     }
 }
+
 extension SignUpViewController: AlertView {
     func showMessage(viewModel: AlertViewModel) {
+        
     }
 }
--- UITests/SignUpViewControllerTests.swift
+++ UITests/SignUpViewControllerTests.swift
@@ -4,7 +4,6 @@
 //
 //  Created by bruno araujo on 19/06/21.
 //
-
 import XCTest
 import UIKit
 import Presentation
@@ -14,22 +13,24 @@ class SignUpViewControllerTests: XCTestCase {
     func test_loading_is_hidden_on_start() {
         XCTAssertEqual(makeSut().loadingIndicator?.isAnimating, false)
     }
-    
+
     func test_sut_implements_loadingView() {
         XCTAssertNotNil(makeSut() as LoadingView)
     }
-    
+
     func test_sut_implements_alertView() {
         XCTAssertNotNil(makeSut() as AlertView)
     }
-    
+
     func test_saveButton_calls_signUp_on_tap() {
-        var callsCount = 0
-        let sut = makeSut(signUpSpy: { _ in
-            callsCount += 1
-        })
+        var signUpViewModel: SignUpViewModel?
+        let sut = makeSut(signUpSpy: { signUpViewModel = $0 })
         sut.saveButton?.simulateTap()
-        XCTAssertEqual(callsCount, 1)
+        let name = sut.nameTextField?.text
+        let email = sut.emailTextField?.text
+        let password = sut.passwordTextField?.text
+        let passwordConfirmation = sut.passwordConfirmationTextField?.text
+        XCTAssertEqual(signUpViewModel, SignUpViewModel(name: name, email: email, password: password, passwordConfirmation: passwordConfirmation))
     }
 }
 
@@ -51,7 +52,7 @@ extension UIControl {
             }
         }
     }
-    
+
     func simulateTap() {
         simulate(event: .touchUpInside)
     }
