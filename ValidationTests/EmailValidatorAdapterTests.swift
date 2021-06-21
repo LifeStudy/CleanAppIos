ba4dfa3f8aa3f9070510886d5aa98eaaf59da3ab
ArjDev69 <bruno96araujo@gmail.com>
2021-06-21 16:22:09 -0300

    add Validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by bruno araujo on 21/06/21.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
